760b87a84a68bc01249eeeb535b68b5697e193d2
robzgf <rgrielfilho@gmail.com>
2020-09-12 16:50:33 -0300

    Conteúdo da aula de operadores

---
+++ introducao/operadores.js
@@ -0,0 +1,17 @@
+let a = 10;
+const b = "10";
+
+console.log(a == b);
+//== Compara o valor de duas expressões
+
+console.log(a === b);
+//=== Compara o valor e o tipo de dados
+
+console.log(a !== b);
+// !==compara se o valor e tipo de dados são diferentes
+
+console.log(a == b && typeof a == 'string');
+// && Operador lógico, verifica uma condição E outra condição em seguida
+
+console.log(a == b || typeof a == 'string');
+// || Operador lógico, verifica uma condição OU outra condição em seguida
