f85f9995a51492e004ff867e646ee28ad99dcb0d
robzgf <rgrielfilho@gmail.com>
2020-09-12 17:25:26 -0300

    Aula com exemplo de formatação de Datas

---
+++ introducao/datas.js
@@ -0,0 +1,10 @@
+let agora = Date.now();
+
+console.log(agora); //Retorna o Stimestamp da data de agora
+
+let data = new Date();
+console.log(data);
+console.log(data.getDate());
+console.log(data.getMonth());
+console.log(data.getFullYear());
+console.log(data.toLocaleDateString("pt-BR"));
